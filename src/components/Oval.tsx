c88aaf6bb5149b9512e3126b17b382d89c02ade5
Kyle Johnson <11kyle.johnson@gmail.com>
2023-11-04 10:50:52 -0500

    added backgrouind images

---
+++ src/components/Oval.tsx
@@ -0,0 +1,5 @@
+export default function Oval() {
+  return (
+    <div className="-z-10 absolute bottom-[124px] right-1/2 w-[510px] h-[1018px] rounded-[255px] bg-gradient-to-tr from-[#8739FF] to-[#E844FF]"></div>
+  )
+}
---
+++ src/components/OvalLight.tsx
@@ -0,0 +1,5 @@
+export default function OvalLight() {
+  return (
+    <div className="-z-10 absolute top-0 -right-1/2 w-[510px] h-[1018px] rounded-[255px] bg-gradient-to-tr from-[#8739FF] to-[#E844FF] opacity-5"></div>
+  )
+}
