b7d2c92ccb50d74fb22cb23513b7d7bb2e75a9de
Kyle Johnson <11kyle.johnson@gmail.com>
2023-11-05 10:16:18 -0600

    layout adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Image from "./components/Image"
 import Message from "./components/Message"
 import Oval from "./components/Oval"
-import OvalLight from "./components/OvalLight"
 import Phone from "./components/Phone"
 import Reminder from "./components/Reminder"
 import Textarea from "./components/Textarea"
@@ -55,8 +54,7 @@ function App() {
           <Textarea></Textarea>
         </Phone>
       </div>
-      <div className="relative">
-        <OvalLight />
+      <div className="relative mb-16">
         <div className="max-w-[445px] flex flex-col gap-y-6 text-center md:text-left mx-8">
           <h1 className="text-[#3E2753] text-[40px] font-medium">Simple booking</h1>
           <p className="text-[#A39DA9] text-[16px] leading-[28px]">Stay in touch with our dog walkers through the chat interface. This makes it easy to discuss arrangements and make bookings. Once the walk has been completed you can rate your walker and book again all through the chat.</p>
